7269283364ea584d25ad40e8309ca5007a05953d
Anthony Agatiello <anthonya1999@gmail.com>
2015-04-28 19:14:42 -0400

    Updated entitlements for debugging and AppDelegate.

--- iRec/AppDelegate.m
+++ iRec/AppDelegate.m
@@ -323,7 +323,6 @@ static NSString * const LastCheckForUpdatesKey = @"lastCheckForUpdates";
     NSLog(@"Started playing file: %@", resourcePath);
 }
 
-
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
@@ -347,11 +346,13 @@ static NSString * const LastCheckForUpdatesKey = @"lastCheckForUpdates";
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     [self stopBackgroundTask];
+    [player stop];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     [self stopBackgroundTask];
+    [player stop];
 }
 
 @end
