e03a126a987f215ede42e3a281ee112484ecc7b1
Anthony Agatiello <anthonya1999@gmail.com>
2015-07-29 21:30:50 -0400

    Moved declarations of data types to separate file.

---
+++ iRec/DataTypes.h
@@ -0,0 +1,52 @@
+//
+//  Declarations.h
+//  iRec
+//
+//  Created by Anthony Agatiello on 7/29/15.
+//  Copyright © 2015 ADA Tech, LLC. All rights reserved.
+//
+
+__BEGIN_DECLS
+
+typedef struct __IOSurface *IOSurfaceRef;
+typedef struct __IOSurfaceAccelerator *IOSurfaceAcceleratorRef;
+typedef void *IOMobileFramebufferConnection;
+
+enum {
+    kIOServiceInteractionAllowed = 0x00000001
+};
+
+enum {
+    kIOSurfaceLockReadOnly  = 0x00000001,
+    kIOSurfaceLockAvoidSync = 0x00000002
+};
+
+enum {
+    kIODefaultCache		 = 0,
+    kIOInhibitCache		 = 1,
+    kIOWriteThruCache    = 2,
+    kIOCopybackCache     = 3,
+    kIOWriteCombineCache = 4
+};
+
+enum {
+    kIOMapAnywhere		    = 0x00000001,
+    kIOMapCacheMask		    = 0x00000700,
+    kIOMapCacheShift		= 8,
+    kIOMapDefaultCache		= kIODefaultCache      << kIOMapCacheShift,
+    kIOMapInhibitCache		= kIOInhibitCache      << kIOMapCacheShift,
+    kIOMapWriteThruCache	= kIOWriteThruCache    << kIOMapCacheShift,
+    kIOMapCopybackCache		= kIOCopybackCache     << kIOMapCacheShift,
+    kIOMapWriteCombineCache	= kIOWriteCombineCache << kIOMapCacheShift,
+    kIOMapUserOptionsMask	= 0x00000fff,
+    kIOMapReadOnly		    = 0x00001000,
+    kIOMapStatic		    = 0x01000000,
+    kIOMapReference		    = 0x02000000,
+    kIOMapUnique		    = 0x04000000
+#ifdef XNU_KERNEL_PRIVATE
+    , kIOMap64Bit		    = 0x08000000
+#endif
+};
+
+__END_DECLS
+
--- iRec/ScreenRecorder.h
+++ iRec/ScreenRecorder.h
@@ -8,51 +8,8 @@
 
 #import <Foundation/Foundation.h>
 #import <AVFoundation/AVFoundation.h>
+#import "DataTypes.h"
 #import <UIKit/UIKit.h>
-
-__BEGIN_DECLS
-
-typedef struct __IOSurface *IOSurfaceRef;
-typedef struct __IOSurfaceAccelerator *IOSurfaceAcceleratorRef;
-typedef void *IOMobileFramebufferConnection;
-
-enum {
-    kIOServiceInteractionAllowed = 0x00000001
-};
-
-enum {
-    kIOSurfaceLockReadOnly  = 0x00000001,
-    kIOSurfaceLockAvoidSync = 0x00000002
-};
-
-enum {
-    kIODefaultCache		 = 0,
-    kIOInhibitCache		 = 1,
-    kIOWriteThruCache    = 2,
-    kIOCopybackCache     = 3,
-    kIOWriteCombineCache = 4
-};
-
-enum {
-    kIOMapAnywhere		    = 0x00000001,
-    kIOMapCacheMask		    = 0x00000700,
-    kIOMapCacheShift		= 8,
-    kIOMapDefaultCache		= kIODefaultCache      << kIOMapCacheShift,
-    kIOMapInhibitCache		= kIOInhibitCache      << kIOMapCacheShift,
-    kIOMapWriteThruCache	= kIOWriteThruCache    << kIOMapCacheShift,
-    kIOMapCopybackCache		= kIOCopybackCache     << kIOMapCacheShift,
-    kIOMapWriteCombineCache	= kIOWriteCombineCache << kIOMapCacheShift,
-    kIOMapUserOptionsMask	= 0x00000fff,
-    kIOMapReadOnly		    = 0x00001000,
-    kIOMapStatic		    = 0x01000000,
-    kIOMapReference		    = 0x02000000,
-    kIOMapUnique		    = 0x04000000
-#ifdef XNU_KERNEL_PRIVATE
-    , kIOMap64Bit		    = 0x08000000
-#endif
-};
-
-__END_DECLS
 
 @interface ScreenRecorder : NSObject
 
